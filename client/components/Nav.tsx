b8ad18f9c02f746e3ab9bf4f96ece0e8e0d61963
Eilish McEwan <eilishmcewan@gmail.com>
2025-04-13 18:15:04 +1200

    nav.tsx

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,19 +1,17 @@
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
+  const { user, logout, loginWithRedirect } = useAuth0()
   // TODO: replace placeholder user object with the one from auth0
-  const user = {
-    nickname: 'john.doe',
-  }
-
   const handleSignOut = () => {
-    console.log('sign out')
+    return logout({})
   }
 
   const handleSignIn = () => {
-    console.log('sign in')
+    return loginWithRedirect({})
   }
 
   return (
